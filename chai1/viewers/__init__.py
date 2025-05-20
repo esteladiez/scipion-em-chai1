d16fc3538120a56c1135864c0657c5e8fbe9c593
esteladiez <100471867@alumnos.uc3m.es>
2025-05-21 03:44:44 +1000

    Update __init__.py

--- chai1/viewers/__init__.py
+++ chai1/viewers/__init__.py
@@ -3,3 +3,4 @@
 # Module to declare viewers
 # Find documentation here: https://scipion-em.github.io/docs/release-3.0.0/docs/developer/tutorials/course_day2.html#writing-a-viewer
 # **************************************************************************
+from .viewer import ChimeraChaiViewer
